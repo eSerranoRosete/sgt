e06042b0a153e9f8270809ec7ff18f42d33917aa
eSerranoRosete <ser20003@byui.edu>
2022-05-12 20:51:38 -0600

    Changed landing page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     extend: {
       animation: {
         'spin-slow': 'spin 45s linear infinite',
-        'pulse-slow': 'pulse 5s linear infinite'
-      } 
+        'pulse-slow': 'pulse 5s linear infinite',
+      }
     },
   },
   plugins: [require('@tailwindcss/forms')],
